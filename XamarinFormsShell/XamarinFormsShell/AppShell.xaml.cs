f7fd6089dc57474eb4fe89a475f8141f95b81be3
Malcolm Jack <inquisitorjax@gmail.com>
2019-01-09 00:46:23 +0200

    init commit

---
+++ XamarinFormsShell/XamarinFormsShell/AppShell.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Xamarin.Forms.Xaml;
+
+namespace XamarinFormsShell
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class AppShell : Xamarin.Forms.Shell
+	{
+		public AppShell()
+		{
+			InitializeComponent();
+		}
+	}
+}
